6e69176da379ddb1e72edf25b622920b041f9496
Kristoffer Sjogren <stoffe@gmail.com>
2015-01-24 23:32:17 +0100

    javadoc for cursor

--- lmdbjni/src/main/java/org/fusesource/lmdbjni/Cursor.java
+++ lmdbjni/src/main/java/org/fusesource/lmdbjni/Cursor.java
@@ -38,6 +38,14 @@ public class Cursor extends NativeObject implements AutoCloseable {
     super(self);
   }
 
+  /**
+   * <p>
+   *   Close a cursor handle.
+   * </p>
+   *
+   * The cursor handle will be freed and must not be used again after this call.
+   * Its transaction must still be live if it is a write-transaction.
+   */
   @Override
   public void close() {
     if (self != 0) {
@@ -46,10 +54,36 @@ public class Cursor extends NativeObject implements AutoCloseable {
     }
   }
 
+  /**
+   * <p>
+   *   Renew a cursor handle.
+   * </p>
+   *
+   * A cursor is associated with a specific transaction and database.
+   * Cursors that are only used in read-only
+   * transactions may be re-used, to avoid unnecessary malloc/free overhead.
+   * The cursor may be associated with a new read-only transaction, and
+   * referencing the same database handle as it was created with.
+   * This may be done whether the previous transaction is live or dead.
+   *
+   * @param tx transaction handle
+   */
   public void renew(Transaction tx) {
     checkErrorCode(mdb_cursor_renew(tx.pointer(), pointer()));
   }
 
+  /**
+   * <p>
+   *   Retrieve by cursor.
+   * </p>
+   * This function retrieves key/data pairs from the database. The address and length
+   * of the key are returned in the object to which \b key refers (except for the
+   * case of the #MDB_SET option, in which the \b key object is unchanged), and
+   * the address and length of the data are returned in the object to which \b data
+   *
+   * @param op A cursor operation #MDB_cursor_op
+   * @return
+   */
   public Entry get(GetOp op) {
     checkArgNotNull(op, "op");
 
@@ -63,6 +97,9 @@ public class Cursor extends NativeObject implements AutoCloseable {
     return new Entry(key.toByteArray(), value.toByteArray());
   }
 
+  /**
+   * @see org.fusesource.lmdbjni.Cursor#get(GetOp)
+   */
   public int position(DirectBuffer key, DirectBuffer value, GetOp op) {
     if (buffer == null) {
       buffer = new DirectBuffer(ByteBuffer.allocateDirect(Unsafe.ADDRESS_SIZE * 4));
@@ -78,6 +115,10 @@ public class Cursor extends NativeObject implements AutoCloseable {
     return rc;
   }
 
+  /**
+   * Same as get but with a seek operation.
+   * @see org.fusesource.lmdbjni.Cursor#get(GetOp)
+   */
   public int seekPosition(DirectBuffer key, DirectBuffer value, SeekOp op) {
     checkArgNotNull(key, "key");
     checkArgNotNull(value, "value");
@@ -105,6 +146,11 @@ public class Cursor extends NativeObject implements AutoCloseable {
     value.wrap(Unsafe.getAddress(bufferAddress, 3), valSize);
   }
 
+  /**
+   * Same as get but with a seek operation.
+   *
+   * @see org.fusesource.lmdbjni.Cursor#get(GetOp)
+   */
   public Entry seek(SeekOp op, byte[] key) {
     checkArgNotNull(key, "key");
     checkArgNotNull(op, "op");
@@ -123,7 +169,54 @@ public class Cursor extends NativeObject implements AutoCloseable {
     }
 
   }
-
+  /**
+   * <p>
+   *   Store by cursor.
+   * </p>
+   *
+   * @param key The key operated on.
+   * @param value The data operated on.
+   * @param flags Options for this operation. This parameter
+   * must be set to 0 or one of the values described here.
+   * <ul>
+   *	<li>#MDB_CURRENT - replace the item at the current cursor position.
+   *		The \b key parameter must still be provided, and must match it.
+   *		If using sorted duplicates (#MDB_DUPSORT) the data item must still
+   *		sort into the same place. This is intended to be used when the
+   *		new data is the same size as the old. Otherwise it will simply
+   *		perform a delete of the old record followed by an insert.
+   *	<li>#MDB_NODUPDATA - enter the new key/data pair only if it does not
+   *		already appear in the database. This flag may only be specified
+   *		if the database was opened with #MDB_DUPSORT. The function will
+   *		return #MDB_KEYEXIST if the key/data pair already appears in the
+   *		database.
+   *	<li>#MDB_NOOVERWRITE - enter the new key/data pair only if the key
+   *		does not already appear in the database. The function will return
+   *		#MDB_KEYEXIST if the key already appears in the database, even if
+   *		the database supports duplicates (#MDB_DUPSORT).
+   *	<li>#MDB_RESERVE - reserve space for data of the given size, but
+   *		don't copy the given data. Instead, return a pointer to the
+   *		reserved space, which the caller can fill in later. This saves
+   *		an extra memcpy if the data is being generated later.
+   *	<li>#MDB_APPEND - append the given key/data pair to the end of the
+   *		database. No key comparisons are performed. This option allows
+   *		fast bulk loading when keys are already known to be in the
+   *		correct order. Loading unsorted keys with this flag will cause
+   *		data corruption.
+   *	<li>#MDB_APPENDDUP - as above, but for sorted dup data.
+   *	<li>#MDB_MULTIPLE - store multiple contiguous data elements in a
+   *		single request. This flag may only be specified if the database
+   *		was opened with #MDB_DUPFIXED. The \b data argument must be an
+   *		array of two MDB_vals. The mv_size of the first MDB_val must be
+   *		the size of a single data element. The mv_data of the first MDB_val
+   *		must point to the beginning of the array of contiguous data elements.
+   *		The mv_size of the second MDB_val must be the count of the number
+   *		of data elements to store. On return this field will be set to
+   *		the count of the number of elements actually written. The mv_data
+   *		of the second MDB_val is unused.
+   * </ul>
+   * @return the value that was stored
+   */
   public byte[] put(byte[] key, byte[] value, int flags) {
     checkArgNotNull(key, "key");
     checkArgNotNull(value, "value");
@@ -140,6 +233,9 @@ public class Cursor extends NativeObject implements AutoCloseable {
     }
   }
 
+  /**
+   * @see org.fusesource.lmdbjni.Cursor#put(byte[], byte[], int)
+   */
   public int put(DirectBuffer key, DirectBuffer value, int flags) {
     checkArgNotNull(key, "key");
     checkArgNotNull(value, "value");
@@ -166,14 +262,39 @@ public class Cursor extends NativeObject implements AutoCloseable {
     return valueSlice.toByteArray();
   }
 
+  /**
+   * <p>
+   *   Delete current key/data pair.
+   * </p>
+   *
+   * This function deletes the key/data pair to which the cursor refers.
+   */
   public void delete() {
     checkErrorCode(mdb_cursor_del(pointer(), 0));
   }
-
+  /**
+   * <p>
+   *   Delete current key/data pair.
+   * </p>
+   *
+   * This function deletes all of the data items for the current key.
+   *
+   * May only be called if the database was opened with #MDB_DUPSORT.
+   */
   public void deleteIncludingDups() {
     checkErrorCode(mdb_cursor_del(pointer(), MDB_NODUPDATA));
   }
 
+  /**
+   * <p>
+   *  Return count of duplicates for current key.
+   * </p>
+   *
+   * This call is only valid on databases that support sorted duplicate
+   * data items #MDB_DUPSORT.
+   *
+   * @return count of duplicates for current key
+   */
   public long count() {
     long rc[] = new long[1];
     checkErrorCode(mdb_cursor_count(pointer(), rc));
